f5f731f81a3daa66e961b26dcd229ffc8f238a7f
Paul Lietar <pl2113@ic.ac.uk>
2024-05-16 15:18:26 +0100

    WIP

--- touchstone/script.R
+++ touchstone/script.R
@@ -10,6 +10,15 @@ touchstone::benchmark_run(
 )
 
 touchstone::benchmark_run(
+  midrange_population = {
+    params <- malariasimulation::get_parameters(
+      overrides = list(human_population=1e5))
+    malariasimulation::run_simulation(10, params)
+  },
+  n = 2
+)
+
+touchstone::benchmark_run(
   large_population = {
     params <- malariasimulation::get_parameters(
       overrides = list(human_population=1e6))
